b65503603e27c216286ebed34b6e56ba7102bee7
Minijb Banks <bminijb@gmail.com>
2024-04-17 02:07:57 -0400

    initial

---
+++ ftpProject/main.py
@@ -0,0 +1,22 @@
+
+# Standard Imports
+import asyncio
+
+# User-Defined Imports
+import eventqueue
+import data
+
+
+# Initialize EventQueue
+events = eventqueue.EventQueue()
+
+
+# Initialize Data
+data_bin = data.Data()
+
+
+# Initialize ftp
+
+
+
+
