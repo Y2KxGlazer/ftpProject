364cf7f6a61cdf6ab0ed67a7e698c76dfc7101b1
Minijb Banks <bminijb@gmail.com>
2024-04-28 00:37:52 -0400

    initial

--- ftpProject/main.py
+++ ftpProject/main.py
@@ -1,10 +1,13 @@
-
 # Standard Imports
-import asyncio
+import pathlib
+
+# External Imports
+import pyftpdlib
 
 # User-Defined Imports
 import eventqueue
 import data
+from ftpserver import UD_Server
 
 
 # Initialize EventQueue
@@ -12,11 +15,31 @@ events = eventqueue.EventQueue()
 
 
 # Initialize Data
-data_bin = data.Data()
+noFile = True
+while noFile:
+    path_to_file = input("Enter a file path")
+    path_to_file = path_to_file.replace('"', '')
+    try:
+        if pathlib.Path(path_to_file).is_file():
+            with open(path_to_file, "rb") as file:
+                events.appendEvent("File Read in Progress")
+                data_bin = data.Data(file)
+                events.appendEvent("Read Success", eventID=123)
+                noFile = False
+
+                #print(data_bin.bin_data.read())
+                break
+        else:
+            print("No File Found!")
+    except Exception as e:
+        print(f"Error: {e}")
+print(events.Event)
+
+# Initialize FTPServer with EventQueue
+n1 = "Clem"
+pw = "1234"
 
 
-# Initialize ftp
-
-
-
-
+server = UD_Server()
+server.add_user(n1, pw)
+server.ftp_start()
